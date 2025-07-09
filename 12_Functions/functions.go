c616185992d2e30f0876e97295cae60b84e9a80b
Vineeth-28 <vineetprakash918@gmail.com>
2025-07-09 04:05:50 -0700

    functions

--- 12_Functions/functions.go
+++ 12_Functions/functions.go
@@ -6,6 +6,13 @@ func add (a int  ,b int ) int { //syntax  func function name (variable datatype 
 	return a +b
 
 }
+
+//same parameters -if the parameters is same it can be declared like this 
+//syntax  func <name of function > (variable 1 , variable 2  datatype ) return type{}
+ func add2(a,b int) int {
+	return a+b
+ }
 func main(){
    fmt.Println( add(3,5))
+   fmt.Println(add2(2,3))
 }
